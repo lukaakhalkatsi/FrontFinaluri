20c47e75d9ee5a97230f0632c91b67ecff0da2cb
Luka <luka.akhalkatsi.1@btu.edu.ge>
2024-06-25 09:47:08 -0700

    create&deletion done

---
+++ app/create.js
@@ -0,0 +1,42 @@
+const createBtn = document.querySelector(".create-news");
+
+createBtn.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const title = document.querySelector(".title").value;
+  const desc = document.querySelector(".desc").value;
+  const category = document.querySelector(".category").value;
+  const fname = document.querySelector(".fname").value;
+  const lname = document.querySelector(".lname").value;
+  const postData = {
+    title,
+    description: desc,
+    category,
+    editorFirstName: fname,
+    editorLastName: lname,
+  };
+  console.log(postData);
+
+  try {
+    const response = await fetch(
+      "https://btu-exam-cb6c3fdf3b9d.herokuapp.com/news",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    } else {
+      window.location.href = "../index.html";
+    }
+
+    const data = await response.json();
+    console.log("Success:", data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
--- app/script.js
+++ app/script.js
@@ -6,6 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const tableBody = document.querySelector(".table-body");
 const loadingBox = document.querySelector(".news-loading");
+const createBtn = document.querySelector(".create");
+
+createBtn.addEventListener("click", function () {
+  window.location.href = "../pages/create.html";
+});
 
 async function fetchNews() {
   loadingBox.classList.remove("hidden");
@@ -28,14 +33,47 @@ async function fetchNews() {
   }
 }
 
+async function deleteNews(id) {
+  const tr = tableBody.querySelector(`tr[data-id="${id}"]`);
+  if (!tr) return;
+
+  tr.classList.add("fade-out");
+  const response = await fetch(
+    `https://btu-exam-cb6c3fdf3b9d.herokuapp.com/news/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  const data = await response.json();
+  console.log(data);
+}
+
+tableBody.addEventListener("click", async (event) => {
+  if (event.target.classList.contains("delete-btn")) {
+    const tr = event.target.closest("tr");
+    const id = tr.getAttribute("data-id");
+
+    try {
+      await deleteNews(id);
+      tr.remove();
+    } catch (error) {
+      console.error("Error deleting news item:", error);
+    }
+  }
+});
+
 function insterDataIntoTable(newsData) {
   newsData.forEach(
-    ({ id, title, category, likes, dateUpdated, dateCreated }, index) => {
+    ({ id, title, category, likes, dateUpdated, dateCreated }) => {
       const formattedDateUpdated = formatDate(dateUpdated);
       const formattedDateCreated = formatDate(dateCreated);
 
       const rowHtml = `
-           <tr>
+           <tr data-id="${id}">
             <td class="table-header-cell text-zinc-700">${id}</td>
             <td class="table-header-cell text-zinc-700">
               ${title}
@@ -45,7 +83,7 @@ function insterDataIntoTable(newsData) {
         <td class="table-header-cell text-zinc-700">${formattedDateUpdated}</td>
         <td class="table-header-cell text-zinc-700">${formattedDateCreated}</td>
             <td class="table-header-cell text-zinc-700 action-td">
-              <button class="action-buttons">Delete</button>
+              <button class="action-buttons delete-btn">Delete</button>
               <button class="action-buttons">Update</button>
             </td>
           </tr>
