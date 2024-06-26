20c1d9b929ea183748706b7c333dc6fe7cefca7a
Luka <luka.akhalkatsi.1@btu.edu.ge>
2024-06-26 08:50:07 -0700

    fixes

--- app/script.js
+++ app/script.js
@@ -33,44 +33,6 @@ createBtn.addEventListener("click", function () {
   window.location.href = "../pages/create.html";
 });
 
-async function fetchNews() {
-  loadingBox.classList.remove("hidden");
-  try {
-    const response = await fetch(
-      "https://btu-exam-cb6c3fdf3b9d.herokuapp.com/news"
-    );
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    const data = await response.json();
-    insterDataIntoTable(data);
-  } catch (error) {
-    console.error("There was a problem with the fetch operation:", error);
-  } finally {
-    loadingBox.classList.add("hidden");
-  }
-}
-
-async function deleteNews(id) {
-  const tr = tableBody.querySelector(`tr[data-id="${id}"]`);
-  if (!tr) return;
-
-  tr.classList.add("slide-out");
-  const response = await fetch(
-    `https://btu-exam-cb6c3fdf3b9d.herokuapp.com/news/${id}`,
-    {
-      method: "DELETE",
-    }
-  );
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  await new Promise((resolve) => setTimeout(resolve, 500));
-  const data = await response.json();
-}
-
 tableBody.addEventListener("click", async (event) => {
   if (event.target.classList.contains("delete-btn")) {
     const tr = event.target.closest("tr");
@@ -107,49 +69,6 @@ tableBody.addEventListener("click", async (event) => {
     }
   }
 });
-
-function insterDataIntoTable(newsData) {
-  newsData?.forEach(
-    ({ id, title, category, likes, dateUpdated, dateCreated }) => {
-      const formattedDateUpdated = formatDate(dateUpdated);
-      const formattedDateCreated = formatDate(dateCreated);
-
-      const rowHtml = `
-           <tr data-id="${id}">
-            <td class="table-header-cell text-zinc-700">${id}</td>
-            <td class="table-header-cell text-zinc-700">
-              ${title}
-            </td>
-            <td class="table-header-cell text-zinc-700">${category}</td>
-            <td class="table-header-cell text-zinc-700">${likes}</td>
-        <td class="table-header-cell text-zinc-700">${formattedDateUpdated}</td>
-        <td class="table-header-cell text-zinc-700">${formattedDateCreated}</td>
-            <td class="table-header-cell text-zinc-700 action-td">
-              <button class="action-buttons delete-btn">Delete</button>
-              <button class="action-buttons update-btn">Update</button>
-            </td>
-          </tr>
-    `;
-      tableBody.insertAdjacentHTML("beforeend", rowHtml);
-    }
-  );
-}
-
-function insertDataIntoPopUp({
-  id,
-  title,
-  description,
-  category,
-  editorFirstName,
-  editorLastName,
-}) {
-  updateID = id;
-  updateTitle.value = title;
-  updateDesc.value = description;
-  updateCategory.value = category;
-  updateEditorFirstName.value = editorFirstName;
-  updateEditorLastName.value = editorLastName;
-}
 
 confirmUpdateBtn.addEventListener("click", async function () {
   const title = updateTitle.value;
@@ -193,3 +112,84 @@ confirmUpdateBtn.addEventListener("click", async function () {
     console.error("Error:", error);
   }
 });
+
+async function fetchNews() {
+  loadingBox.classList.remove("hidden");
+  try {
+    const response = await fetch(
+      "https://btu-exam-cb6c3fdf3b9d.herokuapp.com/news"
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    insterDataIntoTable(data);
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  } finally {
+    loadingBox.classList.add("hidden");
+  }
+}
+
+async function deleteNews(id) {
+  const tr = tableBody.querySelector(`tr[data-id="${id}"]`);
+  if (!tr) return;
+
+  tr.classList.add("slide-out");
+  const response = await fetch(
+    `https://btu-exam-cb6c3fdf3b9d.herokuapp.com/news/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  const data = await response.json();
+}
+
+function insterDataIntoTable(newsData) {
+  newsData?.forEach(
+    ({ id, title, category, likes, dateUpdated, dateCreated }) => {
+      const formattedDateUpdated = formatDate(dateUpdated);
+      const formattedDateCreated = formatDate(dateCreated);
+
+      const rowHtml = `
+           <tr data-id="${id}">
+            <td class="table-header-cell text-zinc-700">${id}</td>
+            <td class="table-header-cell text-zinc-700">
+              ${title}
+            </td>
+            <td class="table-header-cell text-zinc-700">${category}</td>
+            <td class="table-header-cell text-zinc-700">${likes}</td>
+        <td class="table-header-cell text-zinc-700">${formattedDateUpdated}</td>
+        <td class="table-header-cell text-zinc-700">${formattedDateCreated}</td>
+            <td class="table-header-cell text-zinc-700 action-td">
+              <button class="action-buttons delete-btn">Delete</button>
+              <button class="action-buttons update-btn">Update</button>
+            </td>
+          </tr>
+    `;
+      tableBody.insertAdjacentHTML("beforeend", rowHtml);
+    }
+  );
+}
+
+function insertDataIntoPopUp({
+  id,
+  title,
+  description,
+  category,
+  editorFirstName,
+  editorLastName,
+}) {
+  updateID = id;
+  updateTitle.value = title;
+  updateDesc.value = description;
+  updateCategory.value = category;
+  updateEditorFirstName.value = editorFirstName;
+  updateEditorLastName.value = editorLastName;
+}
